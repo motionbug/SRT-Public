f0a3fce7658d9d3bbf7e4d1b0c5958b74d3b3f06
Justin Rummel <j@justinrummel.com>
2013-03-28 16:02:55 -0400

    Updated to point to SRT's official License statement

--- JAMF/EA-RecoveryHD.sh
+++ JAMF/EA-RecoveryHD.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
-# THIS SCRIPT WAS DEVELOPED BY Stony River Technologies
-# Backup, Use at your own risk, it's not our fault for loss of data
-
-# The MIT License
-
-# Copyright (c) 2012 Justin Rummel
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
-# of this software and associated documentation files (the "Software"), to deal 
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
-# THE SOFTWARE.
+# This script was developed BY Stony River Technologies (SRT)
+# ALL scripts are covered by SRT's License found at:
+# https://raw.github.com/stonyrivertech/SRT-Public/master/LICENSE 
 
 # Version 1.0.0 - 9/23/2012
 ### Description 
